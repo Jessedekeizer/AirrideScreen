f248c48ae22a48a77395f234a5d1a06a46c3e7c6
Jessedekeizer <jessedekeizer03@gmail.com>
2025-04-16 12:47:43 +0200

    Create Settings.h

---
+++ AirrideScreen/lib/SettingsStruct/Settings.h
@@ -0,0 +1,25 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+#pragma once
+
+struct Settings
+{
+    double frontMax = 0.0;
+    double backMax = 0.0;
+    double rideFront = 0.0;
+    double rideBack = 0.0;
+    double frontUpX = 0.0;
+    double frontDownX = 0.0;
+    double backUpX = 0.0;
+    double backDownX = 0.0;
+
+    // Helper method to ensure values don't go below 0
+    void adjustValue(double &value, double delta)
+    {
+        value += delta;
+        if (value < 0.0)
+            value = 0.0;
+    }
+};
+
+#endif // SETTINGS_H
