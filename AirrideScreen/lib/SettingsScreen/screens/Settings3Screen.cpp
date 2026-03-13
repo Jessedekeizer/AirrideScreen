5cf834fb144456e64b1ef9d7ffde8c9cd16ec186
Jessedekeizer <jessedekeizer03@gmail.com>
2026-03-13 18:18:04 +0100

    optimize screen draws

--- AirrideScreen/lib/SettingsScreen/screens/Settings3Screen.cpp
+++ AirrideScreen/lib/SettingsScreen/screens/Settings3Screen.cpp
@@ -2,42 +2,57 @@
 
 #include "ESettingsScreenButtons.h"
 
-Settings3Screen::Settings3Screen(ScreenManager &screenManager, SettingsScreenCommunication &settingsScreenCommunication, SettingsDevice &settingsDevice, SettingsStorage &settingsStorage, DisplayService &displayService)
-    : SettingsScreenBase(screenManager, settingsScreenCommunication, settingsDevice, settingsStorage, displayService, EScreen::SETTINGS3, "/Settings3.png") {
-    buttons.push_back(new PushButton(SETTINGSBTN_MAIN_X, SETTINGSBTN_MAIN_Y, SETTINGSBTN_MAIN_W, SETTINGSBTN_MAIN_H, MAIN_SCREEN,
+Settings3Screen::Settings3Screen(ScreenManager &screenManager, SettingsScreenCommunication &settingsScreenCommunication,
+                                 SettingsDevice &settingsDevice, SettingsStorage &settingsStorage,
+                                 DisplayService &displayService)
+    : SettingsScreenBase(screenManager, settingsScreenCommunication, settingsDevice, settingsStorage, displayService,
+                         EScreen::SETTINGS3, "/Settings3.png") {
+    buttons.push_back(new PushButton(SETTINGSBTN_MAIN_X, SETTINGSBTN_MAIN_Y, SETTINGSBTN_MAIN_W, SETTINGSBTN_MAIN_H,
+                                     MAIN_SCREEN,
                                      [this](Button &button) { SaveSettings(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_SAVE_X, SETTINGSBTN_SAVE_Y, SETTINGSBTN_SAVE_W, SETTINGSBTN_SAVE_H, SAVE,
+    buttons.push_back(new PushButton(SETTINGSBTN_SAVE_X, SETTINGSBTN_SAVE_Y, SETTINGSBTN_SAVE_W, SETTINGSBTN_SAVE_H,
+                                     SAVE,
                                      [this](Button &button) { GoToMainScreen(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL1_X, SETTINGSBTN_ADJ_ROW1_Y, SETTINGSBTN_ADJ_W, SETTINGSBTN_ADJ_H, AUTO_RIDE_SECONDS_SUBTRACT,
+    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL1_X, SETTINGSBTN_ADJ_ROW1_Y, SETTINGSBTN_ADJ_W,
+                                     SETTINGSBTN_ADJ_H, AUTO_RIDE_SECONDS_SUBTRACT,
                                      [this](Button &button) { HandleAutoRideSecSub(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL2_X, SETTINGSBTN_ADJ_ROW1_Y, SETTINGSBTN_ADJ_W, SETTINGSBTN_ADJ_H, AUTO_RIDE_SECONDS_ADD,
+    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL2_X, SETTINGSBTN_ADJ_ROW1_Y, SETTINGSBTN_ADJ_W,
+                                     SETTINGSBTN_ADJ_H, AUTO_RIDE_SECONDS_ADD,
                                      [this](Button &button) { HandleAutoRideSecAdd(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL1_X, SETTINGSBTN_ADJ_ROW2_Y, SETTINGSBTN_ADJ_W, SETTINGSBTN_ADJ_H, AUTO_RIDE_ON,
+    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL1_X, SETTINGSBTN_ADJ_ROW2_Y, SETTINGSBTN_ADJ_W,
+                                     SETTINGSBTN_ADJ_H, AUTO_RIDE_ON,
                                      [this](Button &button) { HandleAutoRideOn(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL2_X, SETTINGSBTN_ADJ_ROW2_Y, SETTINGSBTN_ADJ_W, SETTINGSBTN_ADJ_H, AUTO_RIDE_OFF,
+    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL2_X, SETTINGSBTN_ADJ_ROW2_Y, SETTINGSBTN_ADJ_W,
+                                     SETTINGSBTN_ADJ_H, AUTO_RIDE_OFF,
                                      [this](Button &button) { HandleAutoRideOff(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL1_X, SETTINGSBTN_ADJ_ROW3_Y, SETTINGSBTN_ADJ_W, SETTINGSBTN_ADJ_H, AUTO_PARK_SECONDS_SUBTRACT,
+    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL1_X, SETTINGSBTN_ADJ_ROW3_Y, SETTINGSBTN_ADJ_W,
+                                     SETTINGSBTN_ADJ_H, AUTO_PARK_SECONDS_SUBTRACT,
                                      [this](Button &button) { HandleAutoParkSecSub(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL2_X, SETTINGSBTN_ADJ_ROW3_Y, SETTINGSBTN_ADJ_W, SETTINGSBTN_ADJ_H, AUTO_PARK_SECONDS_ADD,
+    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL2_X, SETTINGSBTN_ADJ_ROW3_Y, SETTINGSBTN_ADJ_W,
+                                     SETTINGSBTN_ADJ_H, AUTO_PARK_SECONDS_ADD,
                                      [this](Button &button) { HandleAutoParkSecAdd(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL1_X, SETTINGSBTN_ADJ_ROW4_Y, SETTINGSBTN_ADJ_W, SETTINGSBTN_ADJ_H, AUTO_PARK_ON,
+    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL1_X, SETTINGSBTN_ADJ_ROW4_Y, SETTINGSBTN_ADJ_W,
+                                     SETTINGSBTN_ADJ_H, AUTO_PARK_ON,
                                      [this](Button &button) { HandleAutoParkOn(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL2_X, SETTINGSBTN_ADJ_ROW4_Y, SETTINGSBTN_ADJ_W, SETTINGSBTN_ADJ_H, AUTO_PARK_OFF,
+    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL2_X, SETTINGSBTN_ADJ_ROW4_Y, SETTINGSBTN_ADJ_W,
+                                     SETTINGSBTN_ADJ_H, AUTO_PARK_OFF,
                                      [this](Button &button) { HandleAutoParkOff(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_NAV_LEFT_X, SETTINGSBTN_NAV_BOTTOM_Y, SETTINGSBTN_NAV_SIZE, SETTINGSBTN_NAV_SIZE, SETTINGS2,
+    buttons.push_back(new PushButton(SETTINGSBTN_NAV_LEFT_X, SETTINGSBTN_NAV_BOTTOM_Y, SETTINGSBTN_NAV_SIZE,
+                                     SETTINGSBTN_NAV_SIZE, SETTINGS2,
                                      [this](Button &button) { HandleSettings2(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_NAV_RIGHT_X, SETTINGSBTN_NAV_BOTTOM_Y, SETTINGSBTN_NAV_SIZE, SETTINGSBTN_NAV_SIZE, SETTINGS4,
+    buttons.push_back(new PushButton(SETTINGSBTN_NAV_RIGHT_X, SETTINGSBTN_NAV_BOTTOM_Y, SETTINGSBTN_NAV_SIZE,
+                                     SETTINGSBTN_NAV_SIZE, SETTINGS4,
                                      [this](Button &button) { HandleSettings4(); }));
 }
 
@@ -59,11 +74,17 @@ void Settings3Screen::HandleAutoRideSecSub() {
 }
 
 void Settings3Screen::HandleAutoRideOn() {
+    if (settings.autoRide)
+        return;
+
     settings.autoRide = true;
     displayService.DrawBoolIcon(settings.autoRide, SETTINGS_BOOL_X, SETTINGS_BOOL_Y_ROW2);
 }
 
 void Settings3Screen::HandleAutoRideOff() {
+    if (!settings.autoRide)
+        return;
+
     settings.autoRide = false;
     displayService.DrawBoolIcon(settings.autoRide, SETTINGS_BOOL_X, SETTINGS_BOOL_Y_ROW2);
 }
@@ -79,11 +100,17 @@ void Settings3Screen::HandleAutoParkSecSub() {
 }
 
 void Settings3Screen::HandleAutoParkOn() {
+    if (settings.autoPark)
+        return;
+
     settings.autoPark = true;
     displayService.DrawBoolIcon(settings.autoPark, SETTINGS_BOOL_X, SETTINGS_BOOL_Y_ROW4);
 }
 
 void Settings3Screen::HandleAutoParkOff() {
+    if (!settings.autoPark)
+        return;
+
     settings.autoPark = false;
     displayService.DrawBoolIcon(settings.autoPark, SETTINGS_BOOL_X, SETTINGS_BOOL_Y_ROW4);
 }
--- AirrideScreen/lib/SettingsScreen/screens/Settings4Screen.cpp
+++ AirrideScreen/lib/SettingsScreen/screens/Settings4Screen.cpp
@@ -1,43 +1,57 @@
 #include "Settings4Screen.h"
 #include "ESettingsScreenButtons.h"
 
-Settings4Screen::Settings4Screen(ScreenManager &screenManager, SettingsScreenCommunication &settingsScreenCommunication, SettingsDevice &settingsDevice, SettingsStorage &settingsStorage, DisplayService &displayService)
-    : SettingsScreenBase(screenManager, settingsScreenCommunication, settingsDevice, settingsStorage, displayService, EScreen::SETTINGS4, "/Settings4.png") {
-    buttons.push_back(new PushButton(SETTINGSBTN_MAIN_X, SETTINGSBTN_MAIN_Y, SETTINGSBTN_MAIN_W, SETTINGSBTN_MAIN_H, MAIN_SCREEN,
+Settings4Screen::Settings4Screen(ScreenManager &screenManager, SettingsScreenCommunication &settingsScreenCommunication,
+                                 SettingsDevice &settingsDevice, SettingsStorage &settingsStorage,
+                                 DisplayService &displayService)
+    : SettingsScreenBase(screenManager, settingsScreenCommunication, settingsDevice, settingsStorage, displayService,
+                         EScreen::SETTINGS4, "/Settings4.png") {
+    buttons.push_back(new PushButton(SETTINGSBTN_MAIN_X, SETTINGSBTN_MAIN_Y, SETTINGSBTN_MAIN_W, SETTINGSBTN_MAIN_H,
+                                     MAIN_SCREEN,
                                      [this](Button &button) { GoToMainScreen(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_SAVE_X, SETTINGSBTN_SAVE_Y, SETTINGSBTN_SAVE_W, SETTINGSBTN_SAVE_H, SAVE,
+    buttons.push_back(new PushButton(SETTINGSBTN_SAVE_X, SETTINGSBTN_SAVE_Y, SETTINGSBTN_SAVE_W, SETTINGSBTN_SAVE_H,
+                                     SAVE,
                                      [this](Button &button) { SaveSettings(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL1_X, SETTINGSBTN_ADJ_ROW1_Y, SETTINGSBTN_ADJ_W, SETTINGSBTN_ADJ_H, PARK_DURATION_SECONDS_SUBTRACT,
+    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL1_X, SETTINGSBTN_ADJ_ROW1_Y, SETTINGSBTN_ADJ_W,
+                                     SETTINGSBTN_ADJ_H, PARK_DURATION_SECONDS_SUBTRACT,
                                      [this](Button &button) { HandleParkDurationSub(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL2_X, SETTINGSBTN_ADJ_ROW1_Y, SETTINGSBTN_ADJ_W, SETTINGSBTN_ADJ_H, PARK_DURATION_SECONDS_SUBTRACT,
+    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL2_X, SETTINGSBTN_ADJ_ROW1_Y, SETTINGSBTN_ADJ_W,
+                                     SETTINGSBTN_ADJ_H, PARK_DURATION_SECONDS_SUBTRACT,
                                      [this](Button &button) { HandleParkDurationAdd(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL1_X, SETTINGSBTN_ADJ_ROW2_Y, SETTINGSBTN_ADJ_W, SETTINGSBTN_ADJ_H, LOGGING_ON,
+    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL1_X, SETTINGSBTN_ADJ_ROW2_Y, SETTINGSBTN_ADJ_W,
+                                     SETTINGSBTN_ADJ_H, LOGGING_ON,
                                      [this](Button &button) { HandleLoggingOn(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL2_X, SETTINGSBTN_ADJ_ROW2_Y, SETTINGSBTN_ADJ_W, SETTINGSBTN_ADJ_H, LOGGING_OFF,
+    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL2_X, SETTINGSBTN_ADJ_ROW2_Y, SETTINGSBTN_ADJ_W,
+                                     SETTINGSBTN_ADJ_H, LOGGING_OFF,
                                      [this](Button &button) { HandleLoggingOff(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL1_X, SETTINGSBTN_ADJ_ROW3_Y, SETTINGSBTN_ADJ_W, SETTINGSBTN_ADJ_H, MACHINE_LEARNING_ON,
+    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL1_X, SETTINGSBTN_ADJ_ROW3_Y, SETTINGSBTN_ADJ_W,
+                                     SETTINGSBTN_ADJ_H, MACHINE_LEARNING_ON,
                                      [this](Button &button) { HandleMachineLearningOn(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL2_X, SETTINGSBTN_ADJ_ROW3_Y, SETTINGSBTN_ADJ_W, SETTINGSBTN_ADJ_H, MACHINE_LEARNING_OFF,
+    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL2_X, SETTINGSBTN_ADJ_ROW3_Y, SETTINGSBTN_ADJ_W,
+                                     SETTINGSBTN_ADJ_H, MACHINE_LEARNING_OFF,
                                      [this](Button &button) { HandleMachineLearningOff(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL1_X, SETTINGSBTN_ADJ_ROW4_Y, SETTINGSBTN_ADJ_W, SETTINGSBTN_ADJ_H, BLUETOOTH_ON,
+    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL1_X, SETTINGSBTN_ADJ_ROW4_Y, SETTINGSBTN_ADJ_W,
+                                     SETTINGSBTN_ADJ_H, BLUETOOTH_ON,
                                      [this](Button &button) { HandleBluetoothOn(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL2_X, SETTINGSBTN_ADJ_ROW4_Y, SETTINGSBTN_ADJ_W, SETTINGSBTN_ADJ_H, BLUETOOTH_OFF,
+    buttons.push_back(new PushButton(SETTINGSBTN_ADJ_COL2_X, SETTINGSBTN_ADJ_ROW4_Y, SETTINGSBTN_ADJ_W,
+                                     SETTINGSBTN_ADJ_H, BLUETOOTH_OFF,
                                      [this](Button &button) { HandleBluetoothOff(); }));
 
     // calibration button doesn't follow grid, use defined X while sharing size
     buttons.push_back(new PushButton(SETTINGSBTN_CALIB_X, 112, SETTINGSBTN_ADJ_W, SETTINGSBTN_ADJ_H, CALIBRATION,
                                      [this](Button &button) { HandleCalibration(); }));
 
-    buttons.push_back(new PushButton(SETTINGSBTN_NAV_LEFT_X, SETTINGSBTN_NAV_BOTTOM_Y, SETTINGSBTN_NAV_SIZE, SETTINGSBTN_NAV_SIZE, SETTINGS3,
+    buttons.push_back(new PushButton(SETTINGSBTN_NAV_LEFT_X, SETTINGSBTN_NAV_BOTTOM_Y, SETTINGSBTN_NAV_SIZE,
+                                     SETTINGSBTN_NAV_SIZE, SETTINGS3,
                                      [this](Button &button) { HandleSettings3(); }));
 }
 
@@ -59,31 +73,49 @@ void Settings4Screen::HandleParkDurationSub() {
 }
 
 void Settings4Screen::HandleLoggingOn() {
+    if (settings.logging)
+        return;
+
     settings.logging = true;
     displayService.DrawBoolIcon(settings.logging, SETTINGS_BOOL_X, SETTINGS_BOOL_Y_ROW2);
 }
 
 void Settings4Screen::HandleLoggingOff() {
+    if (!settings.logging)
+        return;
+
     settings.logging = false;
     displayService.DrawBoolIcon(settings.logging, SETTINGS_BOOL_X, SETTINGS_BOOL_Y_ROW2);
 }
 
 void Settings4Screen::HandleMachineLearningOn() {
+    if (settings.machineLearning)
+        return;
+
     settings.machineLearning = true;
     displayService.DrawBoolIcon(settings.machineLearning, SETTINGS_BOOL_X, SETTINGS_BOOL_Y_ROW3);
 }
 
 void Settings4Screen::HandleMachineLearningOff() {
+    if (!settings.machineLearning)
+        return;
+
     settings.machineLearning = false;
     displayService.DrawBoolIcon(settings.machineLearning, SETTINGS_BOOL_X, SETTINGS_BOOL_Y_ROW3);
 }
 
 void Settings4Screen::HandleBluetoothOn() {
+    if (settings.bluetooth)
+        return;
+
     settings.bluetooth = true;
     displayService.DrawBoolIcon(settings.bluetooth, SETTINGS_BOOL_X, SETTINGS_BOOL_Y_ROW4);
 }
 
 void Settings4Screen::HandleBluetoothOff() {
+    if (!settings.bluetooth)
+        return;
+
     settings.bluetooth = false;
     displayService.DrawBoolIcon(settings.bluetooth, SETTINGS_BOOL_X, SETTINGS_BOOL_Y_ROW4);
 }
